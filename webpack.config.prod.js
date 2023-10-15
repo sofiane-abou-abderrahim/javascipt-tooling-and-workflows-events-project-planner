514762ad0238a50168fc2ec59c302f7499bd8227
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-15 22:13:08 +0300

    created webpack.config.prod.js and added build:prod in package.json then run npm run build:prod

---
+++ webpack.config.prod.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'assets', 'scripts'),
+    publicPath: 'assets/scripts/'
+  },
+  devtool: 'cheap-source-map'
+  // devServer: {
+  //   contentBase: './'
+  // }
+};

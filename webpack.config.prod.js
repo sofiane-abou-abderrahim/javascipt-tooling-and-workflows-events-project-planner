387b89066bd6b08fc7a447b09ba77b6a694a9511
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-15 22:29:32 +0300

    changed naming files in scripts to produce output which is production ready and could be pushed to a server

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'production',
   entry: './src/app.js',
   output: {
-    filename: 'app.js',
+    filename: '[contenthash].js',
     path: path.resolve(__dirname, 'assets', 'scripts'),
     publicPath: 'assets/scripts/'
   },
